2ab6ee9806b8d8b18b39defde224135846f56f76
SheylaQuispe <u201620209@upc.edu.pe>
2024-04-29 23:58:09 -0500

    Clase_Ocupacion
    .

--- backend/src/main/java/backend/project/entities/Ocupacion.java
+++ backend/src/main/java/backend/project/entities/Ocupacion.java
@@ -1,4 +1,24 @@
 package backend.project.entities;
+import lombok.*;
 
-public class Ocupacion {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Builder
+@Table(name ="Ocupacion")
+public class Ocupacion implements Serializable {
+
+    @Id
+    @Column(name ="id_Ocupacion")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_Ocupacion;
+    @Column(name ="nombre_Ocupacion",nullable = false, length = 50)
+    private String nombre_Ocupacion;
+    @Column(name ="descripcion",nullable = false, length = 50)
+    private String descripcion;
 }
