993de4555ffdd5707cccfa37342a52726506bdf3
SheylaQuispe <u201620209@upc.edu.pe>
2024-04-30 00:27:34 -0500

    Clase_ContratoXTrabajador
    .

--- backend/src/main/java/backend/project/entities/ContratoXTrabajador.java
+++ backend/src/main/java/backend/project/entities/ContratoXTrabajador.java
@@ -1,4 +1,25 @@
 package backend.project.entities;
 
-public class ContratoXTrabajador {
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Builder
+@Table(name ="ContratoXTrabajador")
+public class ContratoXTrabajador implements Serializable {
+    @Id
+    @Column(name = "id_ContratoXTrabajador")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_ContratoXTrabajador;
+    @Column(name = "estado_ContratoXTrabajador",nullable = false,length = 100)
+    private String estado_ContratoXTrabajador;
+    @Column(name="descripcion_actividad",nullable = false,length = 100)
+    private String descripcion_actividad;
+
 }
