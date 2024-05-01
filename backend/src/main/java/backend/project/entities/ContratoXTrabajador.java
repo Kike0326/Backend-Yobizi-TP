e4b6dacd221a0df7e7e4c5563c4feade7fc8c663
SheylaQuispe <u201620209@upc.edu.pe>
2024-04-30 21:21:15 -0500

    actualizacion entidad contratoxtrabajador
    .

--- backend/src/main/java/backend/project/entities/ContratoXTrabajador.java
+++ backend/src/main/java/backend/project/entities/ContratoXTrabajador.java
@@ -17,9 +17,14 @@ public class ContratoXTrabajador implements Serializable {
     @Column(name = "id_ContratoXTrabajador")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_ContratoXTrabajador;
-    @Column(name = "estado_ContratoXTrabajador",nullable = false,length = 100)
-    private String estado_ContratoXTrabajador;
+    @Column(name = "estado_ContratoXTrabajador",nullable = false)
+    private boolean estado_ContratoXTrabajador;
     @Column(name="descripcion_actividad",nullable = false,length = 100)
     private String descripcion_actividad;
-
+    @ManyToOne
+    @JoinColumn(name="id_Contrato",nullable = false)
+    private Contrato contrato;
+    @ManyToOne
+    @JoinColumn(name="id_Ocupacion", nullable = false)
+    private Ocupacion ocupacion;
 }
