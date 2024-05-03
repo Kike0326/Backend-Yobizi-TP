13468f1b7e3a231d87a063499daefcec05a48c84
Roberto Alonso <robertojs_03@hotmail.com>
2024-05-02 20:31:19 -0500

    Actualizacion Controllers Distrito y Contrato

---
+++ backend/src/main/java/backend/project/dtos/TrabajadorDTO.java
@@ -0,0 +1,39 @@
+package backend.project.dtos;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Builder
+@Table(name = "Trabajador")
+public class TrabajadorDTO implements Serializable {
+    @Id
+    @Column(name = "id_Trabajador")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_Trabajador;  // recomendable idTrabajador
+
+    @Column(name = "nombre_Trabajador",nullable = false, length = 50)
+    private String nombre_Trabajador;
+    @Column(name = "apellido_Empleador",nullable = false, length = 50)
+    private String apellido_Empleador;
+    @Column(name = "fechaNacimiento_Trabaja",nullable = false)
+    private Date fechaNacimiento_Trabaja;
+    @Column(name = "direccion_Trabajador",nullable = false, length = 50)
+    private String direccion_Trabajador;
+    @Column(name = "correo_Trabajador",nullable = false,length = 50)
+    private String correo_Trabajador;
+    @Column(name = "celular_Trabajador",nullable = false, length = 50)
+    private double celular_Trabajador;
+    @Column(name = "Asesor_id_Asesor")
+    private Integer asesor_id_Asesor;
+    @Column(name = "estado_Trabajador")
+    private boolean estado_Trabajador;
+    @Column(name = "users_id")
+    private Integer users_id;
+}
--- backend/src/main/java/backend/project/repositories/ContratoRepository.java
+++ backend/src/main/java/backend/project/repositories/ContratoRepository.java
@@ -2,5 +2,5 @@ package backend.project.repositories;
 import backend.project.entities.Contrato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContratoRepository extends JpaRepository<Contrato, Integer> {
+public interface    ContratoRepository extends JpaRepository<Contrato, Integer> {
 }
